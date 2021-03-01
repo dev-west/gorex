ef910a851504dd3b4af8cc83c904e884c5bebb9e
dev-west <74560179+dev-west@users.noreply.github.com>
2021-03-01 07:31:21 -0800

    Added all defined classes and quantifiers; changed to pointer methods instead of object methods.

--- gorex.go
+++ gorex.go
@@ -11,16 +11,16 @@
 //  var rex = gorex.GolangExpression()
 //  rex.AddClass(gorex.Uppers)                // add group ([A-Z])
 //  rex.AddClassToLast(gorex.Lowers)          // modify group ([A-Za-z])
-//  rex.AddClassToLast(gorex.Numbers)         // modify group ([A-Za-z0-9])
-//  rex.ApplyQuantityToLast(gorex.OneOrMore)  // modify group ([A-Za-z0-9]+)
+//  rex.AddClassToLast(gorex.Digits)          // modify group ([A-Za-z0-9])
+//  rex.ApplyQuantifier(gorex.OneOrMore)      // modify group ([A-Za-z0-9]+)
 //  rex.AddFixed(".")                         // add group (.)
 //  rex.AddFixedToLast("_")                   // modify group (.|_)
-//  rex.ApplyQuantityToLast(gorex.ZeroOrOne)  // modify group (.|_?)
+//  rex.ApplyQuantifier(gorex.ZeroOrOne)      // modify group (.|_?)
 //  rex.AddClass(gorex.Alphanumerics)         // add group ([A-Za-z0-9]+)
-//  rex.ApplyQuantityToLast(gorex.ZeroOrOne)
+//  rex.ApplyQuantifier(gorex.ZeroOrOne)
 //  rex.AddFixed("@")                         // add group (@)
 //  rex.AddClass(gorex.Alphanumerics)         // add group ([A-Za-z0-9]+)
-//  rex.ApplyQuantityToLast(gorex.OneOrMore)
+//  rex.ApplyQuantifier(gorex.OneOrMore)
 //  rex.AddFixed(".")                         // add group (.)
 //  rex.AddFixed("com")                       // add group (com)
 //  rex.AddFixedToLast("net")                 // modify gorup (com|net)
@@ -39,11 +39,13 @@ package gorex
 import (
 	"bytes"
 	"errors"
-	"strconv"
+	"fmt"
+	"regexp"
 )
 
 type Gorex struct {
 	groups []rexGroup // expression details
+	unsafe bool
 }
 
 type rexGroup struct {
@@ -52,100 +54,121 @@ type rexGroup struct {
 
 type rexToken struct {
 	fixed string
-	class int
-	quantity rexQuan
+	class string
+	quantifier rexQuan
 }
 
+// token quantifiers (variable data)
 type rexQuan struct {
-	flag int
-	start int
-	end int
+	regexp Quantifier
+	argv [2]int
 }
 
-// token quantifiers
-const (
-	Single int = 0
-	ZeroOrOne int = -1
-	ZeroOrMore int = -2
-	OneOrMore int = -3
-)
-
-// token classes
-const (
-	NoClass int =     1 >> 1
-	Uppers int =      1 << 0   // [A-Z]
-	Lowers int =      1 << 1   // [a-z]
-	Numbers int =     1 << 2   // [0-9]
-	Whitespaces int = 1 << 3   // [ \t\n\r]
-	Punctuation int = 1 << 4   // [][!\x22#$%&\x27()*+,./:;<=>?@_\x60{|}~-]
-	AlphaNumerics int = Uppers + Lowers + Numbers
-)
-
-var eG Gorex = Gorex{ }
-
-const validClasses int = Uppers + Lowers + Numbers + Whitespaces + Punctuation
+// quantifier definitions
+type Quantifier string
 
 const (
-	empty string = ""
-	num string = "0-9"
-	lower string = "a-z"
-	upper string = "A-Z"
-	whitespace string = "[ \t]"
-	punctuation string = "[][!\x22#$%&\x27()*+,./:;<=>?@_\x60{|}~-]"
+	Single Quantifier = ""
+	ZeroOrMore Quantifier = "*"
+	OneOrMore Quantifier = "+"
+	ZeroOrOne Quantifier = "?"
+	MinToMax Quantifier = "{%d,%d}"
+	MinOrMore Quantifier = "{%d,}"
+	Exactly Quantifier = "{%d}"
+	ZeroOrMorePrefFewer Quantifier = "*?"
+	OneOrMorePrefFewer Quantifier = "+?"
+	ZeroOrOnePrefFewer Quantifier = "??"
+	MinToMaxPrefFewer Quantifier = "{%d,%d}?"
+	MinOrMorePrefFewer Quantifier = "{%d,}?"
+	ExactlyPrefFewer Quantifier = "{%d}?"
 )
 
-func GolangExpression() (Gorex, error) {
+// class definitions
+const (
+	NoClass string = ""
+	Ascii string = "\x00-\x7F"
+	Blank string = "\t "
+	Control string = "\x00-\x1F\x7F"
+	Digits string = "0-9"
+	Graphical string = "!-~"
+	Lowers string = "a-z"
+	Printable string = " -~"
+	Punctuation string = "!-/:-@[-`{-~"
+	Whitespace string = "\t\n\v\f\r "
+	Uppers string = "A-Z"
+	Words string = "0-9A-Za-z_"
+	HexDigits string = "0-9A-Fa-f"
+	AlphaNumerics string = "0-9A-Za-z"
+	Alphabetics string = "A-Za-z"
+)
+
+var eG *Gorex = &Gorex{ }
+
+// options
+const (
+	Unsafe string = "Unsafe"
+)
+
+func GolangExpression(opts ...string) (*Gorex, error) {
 	var r Gorex
+	r.unsafe = false
+	if len(opts) > 1 {
+		return eG, errors.New("Gorex @116: invalid GolangExpression options")
+	} else {
+		for _, op := range(opts) {
+			if op == "Unsafe" {
+				r.unsafe = true
+				continue
+			}
 
-	return r, nil
+			return eG, errors.New("Gorex @124: invalid GolangExpression options")
+		}
+	}
+
+	return &r, nil
 }
 
-func (g Gorex) Output() (string, error) {
+func (g *Gorex) Output() (string, error) {
 	o := bytes.NewBufferString("")
 	for _, gr := range(g.groups) {
 		o.WriteString("(")
 
 		// add token data
 		for i, tk := range(gr.tokens) {
-			if tk.class != 0 {
+			if tk.class != NoClass {
 				if len(tk.fixed) != 0 {
-					return "", errors.New("Gorex @112: invalid token error")
+					return "", errors.New("Gorex @140: invalid token error")
 				}
 				o.WriteString("[")
-				if tk.class & Uppers != 0 { o.WriteString(upper) }
-				if tk.class & Lowers != 0 {	o.WriteString(lower) }
-				if tk.class & Numbers != 0 { o.WriteString(num) }
-				if tk.class & Whitespaces != 0 { o.WriteString(whitespace) }
-				if tk.class & Punctuation != 0 { o.WriteString(punctuation) }
+				o.WriteString(tk.class)
 				o.WriteString("]")
 			}
 
 			if len(tk.fixed) != 0 {
-				if tk.class != 0 {
-					return "", errors.New("Gorex @125: invalid token error")
+				if tk.class != NoClass {
+					return "", errors.New("Gorex @149: invalid token error")
 				}
 				o.WriteString(tk.fixed)
 				if(len(gr.tokens) > i + 1) { o.WriteString("|") }
 			}
 
 			// add class quantity
-			switch tk.quantity.flag {
-			case Single: break
-			case ZeroOrOne:	o.WriteString("?")
-			case ZeroOrMore: o.WriteString("*")
-			case OneOrMore: o.WriteString("+")
-			default: return "", errors.New("Gorex @137: invalid quantity error")
+			argCount := regexp.MustCompile("%d") // only permits numbers
+			var argc int
+			if argCount.FindAllString(string(tk.quantifier.regexp), -1) == nil {
+				argc = 0
+			} else {
+				argc = len(argCount.FindAllString(string(tk.quantifier.regexp), -1))
 			}
-
-			if tk.quantity.end != 0 {
-				if tk.quantity.start > tk.quantity.end {
-					return "", errors.New("Gorex @142: invalid quantity error")
-				}
-				o.WriteString("{")
-				o.WriteString(strconv.Itoa(tk.quantity.start))
-				o.WriteString(",")
-				o.WriteString(strconv.Itoa(tk.quantity.end))
-				o.WriteString("}")
+			switch(argc) {
+			case 0:
+				o.WriteString(fmt.Sprintf(string(tk.quantifier.regexp)))
+			case 1:
+				o.WriteString(fmt.Sprintf(string(tk.quantifier.regexp), tk.quantifier.argv[0]))
+			case 2:
+				o.WriteString(fmt.Sprintf(string(tk.quantifier.regexp), tk.quantifier.argv[0], tk.quantifier.argv[1]))
+			default:
+				return "", errors.New("Gorex @171: invalid argument count")
 			}
 		}
 
@@ -155,30 +178,64 @@ func (g Gorex) Output() (string, error) {
 	return o.String(), nil
 }
 
-func (g Gorex) AddClass(c int) (Gorex, error) {
-	if c & validClasses == 0 { return eG, errors.New("Gorex @159: invalid class value") }
+func verifyClass(a string) bool {
+	if		a == NoClass ||
+			a == Ascii ||
+			a == Blank ||
+			a == Control ||
+			a == Digits ||
+			a == Graphical ||
+			a == Lowers ||
+			a == Printable ||
+			a == Punctuation ||
+			a == Whitespace ||
+			a == Uppers ||
+			a == Words ||
+			a == HexDigits ||
+			a == AlphaNumerics ||
+			a == Alphabetics {
+		return true
+	}
+
+	return false
+}
+
+func (g *Gorex) AddClass(c string) (*Gorex, error) {
+	if c == NoClass {
+		return eG, errors.New("Gorex @205: invalid class")
+	}
+
+	if !g.unsafe { // is safe
+		if !verifyClass(c) { return eG, errors.New("Gorex @209: invalid class") }
+	}
+
 	id := len(g.groups)
 	r := rexGroup{ }
 	g.groups = append(g.groups, r)
+
 	g.groups[id].tokens = append(g.groups[id].tokens, rexToken { "", c, rexQuan{ } } )
 
 	return g, nil
 }
 
-func (g Gorex) AddClassToLast(c int) (Gorex, error) {
-	if c & validClasses == 0 { return eG, errors.New("Gorex @169: invalid class value") }
-	if(len(g.groups) == 0) { return eG, errors.New("Gorex @170: invalid group index") }
+func (g *Gorex) AddClassToLast(c string) (*Gorex, error) {
+	if !g.unsafe { // is safe
+		if !verifyClass(c) { return eG, errors.New("Gorex @223: invalid class") }
+	}
+
+	if len(g.groups) == 0 { return eG, errors.New("Gorex @226: invalid group index") }
 	gId := len(g.groups) - 1
-	if len(g.groups[gId].tokens) == 0 { return eG, errors.New("Gorex @172: invalid token index") }
+	if len(g.groups[gId].tokens) == 0 { return eG, errors.New("Gorex @228: invalid token index") }
 	tId := len(g.groups[gId].tokens) - 1
+
 	g.groups[gId].tokens[tId].class = g.groups[gId].tokens[tId].class + c
 
 	return g, nil
 }
 
-func (g Gorex) AddFixed(a string) (Gorex, error) {
+func (g *Gorex) AddFixed(a string) (*Gorex, error) {
 	for _, b := range(a) {
-		if byte(b) >= 128 { return eG, errors.New("Gorex @181: invalid byte error") }
+		if byte(b) >= 128 { return eG, errors.New("Gorex @238: invalid byte error") }
 	}
 	id := len(g.groups)
 	r := rexGroup{ }
@@ -188,35 +245,67 @@ func (g Gorex) AddFixed(a string) (Gorex, error) {
 	return g, nil
 }
 
-func (g Gorex) AddFixedToLast(a string) (Gorex, error) {
+func (g *Gorex) AddFixedToLast(a string) (*Gorex, error) {
 	for _, b := range(a) {
-		if byte(b) >= 128 { return eG, errors.New("Gorex @193: invalid byte value") }
+		if byte(b) >= 128 { return eG, errors.New("Gorex @250: invalid byte value") }
 	}
-	if(len(g.groups) == 0) { return eG, errors.New("Gorex @195: invalid group index") }
+	if(len(g.groups) == 0) { return eG, errors.New("Gorex @252: invalid group index") }
 	id := len(g.groups) - 1
-	g.groups[id].tokens = append(g.groups[id].tokens, rexToken { a, NoClass, rexQuan{ } } )
+	g.groups[id].tokens = append(g.groups[id].tokens, rexToken{ a, NoClass, rexQuan{ } } )
 
 	return g, nil
 }
 
-func (g Gorex) ApplyQuantityToLast(params ...int) (Gorex, error) {
-	if len(g.groups) == 0 { return eG, errors.New("Gorex @203: invalid group index") }
-	gId := len(g.groups) - 1
-	if len(g.groups[gId].tokens) == 0 { return eG, errors.New("Gorex @205: invalid token index") }
-	tId := len(g.groups[gId].tokens) - 1
-	if g.groups[gId].tokens[tId].class == 0 && len(g.groups[gId].tokens[tId].fixed) == 0 { return eG, errors.New("Gorex @238: invalid quantifier") }
-	if len(params) == 0 || len(params) > 2 { return eG, errors.New("Gorex @208: invalid quantifier") }
-	switch(len(params)) {
+func verifyQuantifier(q Quantifier, args []int) bool {
+	argc := len(args)
+	if argc > 2 { return false }
+	switch(argc) {
+	case 0:
+		if		q == Single ||
+				q == ZeroOrMore ||
+				q == OneOrMore ||
+				q == ZeroOrOne ||
+				q == ZeroOrMorePrefFewer ||
+				q == OneOrMorePrefFewer ||
+				q == ZeroOrOnePrefFewer {
+			return true }
 	case 1:
-		if params[0] >= 0 { return eG, errors.New("Gorex @211: invalid quantifier") }
-		g.groups[gId].tokens[tId].quantity.flag = params[0]
-		g.groups[gId].tokens[tId].quantity.start = 0
-		g.groups[gId].tokens[tId].quantity.end = 0
+		if		q == MinOrMore ||
+				q == Exactly ||
+				q == MinOrMorePrefFewer ||
+				q == ExactlyPrefFewer {
+			return true }
 	case 2:
-		if params[0] < 0 || params[1] <= 0 { return eG, errors.New("Gorex @216: invalid quantifier") }
-		g.groups[gId].tokens[tId].quantity.flag = 0
-		g.groups[gId].tokens[tId].quantity.start = params[0]
-		g.groups[gId].tokens[tId].quantity.end = params[1]
+		if		q == MinToMax ||
+				q == MinToMaxPrefFewer {
+			return true }
+	}
+
+	return false
+}
+
+func (g *Gorex) ApplyQuantifier(q Quantifier, args ...int) (*Gorex, error) {
+	if string(q) == "" { return eG, errors.New("Gorex @288: invalid quantifier") }
+	if len(g.groups) == 0 { return eG, errors.New("Gorex @289: invalid group index") }
+	gId := len(g.groups) - 1
+	if len(g.groups[gId].tokens) == 0 { return eG, errors.New("Gorex @291: invalid token index") }
+	tId := len(g.groups[gId].tokens) - 1
+	if g.groups[gId].tokens[tId].class == NoClass && len(g.groups[gId].tokens[tId].fixed) == 0 { return eG, errors.New("Gorex @293: invalid quantifier") }
+	if len(args) > 2 { return eG, errors.New("Gorex @294: invalid quantifier") }
+	if !verifyQuantifier(q, args) { return eG, errors.New("Gorex @295: invalid quantifier") }
+
+	g.groups[gId].tokens[tId].quantifier.regexp = q
+
+	switch(len(args)) {
+	case 0:
+		g.groups[gId].tokens[tId].quantifier.argv[0] = 0
+		g.groups[gId].tokens[tId].quantifier.argv[1] = 0
+	case 1:
+		g.groups[gId].tokens[tId].quantifier.argv[0] = args[0]
+		g.groups[gId].tokens[tId].quantifier.argv[1] = 0
+	case 2:
+		g.groups[gId].tokens[tId].quantifier.argv[0] = args[0]
+		g.groups[gId].tokens[tId].quantifier.argv[1] = args[1]
 	}
 
 	return g, nil
