3c56dc91807f7e19ff42fa4d4f63081b2f411002
dev-west <74560179+dev-west@users.noreply.github.com>
2021-03-01 07:32:33 -0800

    Updates match gorex.go

--- example_main.go
+++ example_main.go
@@ -7,44 +7,62 @@ import(
 )
 
 func main() {
-    var g Gorex
+    var g *Gorex
     var e error
 
     validEmails := [...]string{ "joe@mail.org", "john_doe@co.net", "perry.@place.com" }
     invalidEmails := [...]string{ "_tobby@message.org", "goat@mail", "finn@.net" }
 
-    if g, e = GolangExpression(); e != nil { fmt.Println("ExampleEmail failed to create Gorex") }
+    if g, e = GolangExpression(); e != nil { fmt.Println("ExampleEmail failed to create Gorex: %s", e) }
 
-    if g, e = g.AddClass(Uppers); e != nil { fmt.Println("ExampleEmail failed to add Uppers") }
-    if g, e = g.AddClassToLast(Lowers); e != nil { fmt.Println("ExampleEmail failed to add Lowers") }
-    if g, e = g.AddClassToLast(Numbers); e != nil { fmt.Println("ExampleEmail failed to add Numbers") }
-    if g, e = g.ApplyQuantityToLast(OneOrMore); e != nil { fmt.Println("ExampleEmail failed to apply quantity OneOrMore") }
+    if g, e = g.AddClass(Uppers); e != nil { fmt.Println("ExampleEmail failed to add Uppers: %s", e) }
+    if g, e = g.AddClassToLast(Lowers); e != nil { fmt.Println("ExampleEmail failed to add Lowers: %s", e) }
+    if g, e = g.AddClassToLast(Digits); e != nil { fmt.Println("ExampleEmail failed to add Numbers %s", e) }
+    if g, e = g.ApplyQuantifier(OneOrMore); e != nil { fmt.Println("ExampleEmail failed to apply quantity OneOrMore %s", e) }
 
-    if g, e = g.AddFixed("."); e != nil { fmt.Println("ExampleEmail failed to add \".\"") }
-    if g, e = g.AddFixedToLast("_"); e != nil { fmt.Println("ExampleEmail failed to add \"_\"") }
-    if g, e = g.ApplyQuantityToLast(ZeroOrOne); e != nil { fmt.Println("ExampleEmail failed to apply ZeroOrOne") }
+    if g, e = g.AddFixed("."); e != nil { fmt.Println("ExampleEmail failed to add \".\": %s", e) }
+    if g, e = g.AddFixedToLast("_"); e != nil { fmt.Println("ExampleEmail failed to add \"_\": %s", e) }
+    if g, e = g.ApplyQuantifier(ZeroOrOne); e != nil { fmt.Println("ExampleEmail failed to apply ZeroOrOne: %s", e) }
 
-    if g, e = g.AddClass(AlphaNumerics); e != nil { fmt.Println("ExampleEmail failed to add Alphanumerics") }
-    if g, e = g.ApplyQuantityToLast(ZeroOrMore); e != nil { fmt.Println("ExampleEmail failed to apply ZeroOrMore") }
+    if g, e = g.AddClass(AlphaNumerics); e != nil { fmt.Println("ExampleEmail failed to add Alphanumerics: %s", e) }
+    if g, e = g.ApplyQuantifier(ZeroOrMore); e != nil { fmt.Println("ExampleEmail failed to apply OneOrZero: %s", e) }
 
-    if g, e = g.AddFixed("@"); e != nil { fmt.Println("ExampleEmail failed to add \"@\"") }
+    if g, e = g.AddFixed("@"); e != nil { fmt.Println("ExampleEmail failed to add \"@\": %s", e) }
 
-    if g, e = g.AddClass(AlphaNumerics); e != nil { fmt.Println("ExampleEmail failed to add Alphanumerics") }
-    if g, e = g.ApplyQuantityToLast(OneOrMore); e != nil { fmt.Println("ExampleEmail failed to apply OneOrZero") }
+    if g, e = g.AddClass(AlphaNumerics); e != nil { fmt.Println("ExampleEmail failed to add Alphanumerics: %s", e) }
+    if g, e = g.ApplyQuantifier(OneOrMore); e != nil { fmt.Println("ExampleEmail failed to apply OneOrZero: %s", e) }
 
-    if g, e = g.AddFixed("."); e != nil { fmt.Println("ExampleEmail failed to add \".\"") }
+    if g, e = g.AddFixed("."); e != nil { fmt.Println("ExampleEmail failed to add \".\": %s", e) }
 
-    if g, e = g.AddFixed("com"); e != nil { fmt.Println("ExampleEmail failed to add \"com\"") }
-    if g, e = g.AddFixedToLast("net"); e != nil { fmt.Println("ExampleEmail failed to add \"net\"") }
-    if g, e = g.AddFixedToLast("org"); e != nil { fmt.Println("ExampleEmail failed to add \"org\"") }
+    if g, e = g.AddFixed("com"); e != nil { fmt.Println("ExampleEmail failed to add \"com\": %s", e) }
+    if g, e = g.AddFixedToLast("net"); e != nil { fmt.Println("ExampleEmail failed to add \"net\": %s", e) }
+    if g, e = g.AddFixedToLast("org"); e != nil { fmt.Println("ExampleEmail failed to add \"org\": %s", e) }
 
     var exp string
     var r string
-    if exp, e = g.Output(); e != nil { fmt.Println("ExampleEmail failed to Output") }
+    if exp, e = g.Output(); e != nil { fmt.Println("ExampleEmail failed to Output: %s", e) }
 
     var rex = regexp.MustCompile(exp)
 
-    fmt.Printf("Expression: %s\n", exp)
+    fmt.Printf("\nExpression: %s\n", exp)
+
+    for _, r = range(validEmails) {
+        fmt.Printf("Attempt: %s, value: %#v\n", r, rex.MatchString(r))
+    }
+
+    for _, r = range(invalidEmails) {
+        fmt.Printf("Attempt: %s, value: %#v\n", r, rex.MatchString(r))
+    }
+
+	g, _ = GolangExpression()
+	g.AddFixed("n")
+    if g, e = g.ApplyQuantifier(MinToMax, 2, 3); e != nil { fmt.Println("ExampleEmail failed to apply MinToMax, %d, %d: %s", 2, 3, e) }
+
+    if exp, e = g.Output(); e != nil { fmt.Println("ExampleEmail failed to Output: %s", e) }
+
+	rex = regexp.MustCompile(exp)
+
+    fmt.Printf("\nExpression: %s\n", exp)
 
     for _, r = range(validEmails) {
         fmt.Printf("Attempt: %s, value: %#v\n", r, rex.MatchString(r))
